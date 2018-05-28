ac438692f47a57daf28bd5ed285b92e0f3ea4350
Randall Wilk <wilk.randall@gmail.com>
2018-05-28 00:06:02 -0500

    Setup build process

---
+++ build/webpack.mix.js
@@ -0,0 +1,8 @@
+const { mix } = require('laravel-mix');
+const inProduction = mix.inProduction();
+
+mix
+	.setPublicPath('dist')
+	.js('src/index.js', 'index.js')
+	.sass('src/scss/app.scss', 'styles.css')
+	.sourceMaps(!inProduction);
